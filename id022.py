4943c0c7f8bf79e8d58fa5e771c8cfb29a4be794
lukespademan <info@lukespademan.com>
2018-04-21 17:16:51 +0100

    solved id 22

--- id022.py
+++ id022.py
@@ -1,12 +1,20 @@
 """
 Author: Luke Spademan
 Calculates answer to project euler problem idx
-https://projecteuler.net/problem=x
+https://projecteuler.net/problem=22
 """
-
+from utils.word_value import word_value
 
 def calculate():
     ans = 0
+    with open("names.txt") as f:
+       names = f.read()
+       names = names.replace('"','').replace(',', ' ').split()
+       names.sort()
+       for i, name in enumerate(names):
+           value = (i+1) * word_value(name)
+           ans += value
+
     return ans
 
 if __name__ == "__main__":
